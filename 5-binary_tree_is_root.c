368edbdc0e930b2b7f591cdd6a91c8f687e95f72
Arsany199 <arsanykamal30@gmail.com>
2024-01-31 10:09:08 -0800

    5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check for node if it root
+ * @node: is the node to be checked
+ *
+ * Return: 1 if root 0 if not
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (node->left && node->right)
+		return (1);
+	return (0);
+}
