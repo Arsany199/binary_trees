55a0630431486afa04d30ff7b04c6f754f38f288
Arsany199 <arsanykamal30@gmail.com>
2024-01-31 14:01:41 -0800

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: pointer to the root
  * Return: the number of nodes with at least 1 child
  */
